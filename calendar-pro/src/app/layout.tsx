7bb8e22e2e465381f2fc1f1aa189e951be5185fc
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-22 21:53:52 +0530

    feat : added task context provider
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

--- calendar-pro/src/app/layout.tsx
+++ calendar-pro/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DateContextProvider from "@/context/DateContext";
+import TaskContextProvider from "@/context/TaskContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <DateContextProvider>
-        <body className={inter.className}>{children}</body>
+        <TaskContextProvider>
+          <body className={inter.className}>{children}</body>
+        </TaskContextProvider>
       </DateContextProvider>
     </html>
   );
