f87d5e1820366706890a3d44a15ce677ffa8f629
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-28 16:47:49 +0530

    removed unnecessary imports
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

--- calendar-pro/src/components/TimeBox.tsx
+++ calendar-pro/src/components/TimeBox.tsx
@@ -1,10 +1,5 @@
-import { Task, useTaskContext } from "@/context/TaskContext";
-import { formatDate } from "@/helpers/timefunctions";
-import React from "react";
-import TaskDisplay from "./TaskDisplay";
+import React, { useEffect, useState } from "react";
 import { CreateTaskType } from "./WeekGrid";
-import { StructuredTaskType } from "./DayView";
-import TaskComponent from "./TaskComponent";
 
 type TimeBoxProps = {
   timeInterval: {
@@ -14,7 +9,6 @@ type TimeBoxProps = {
   day: Date;
   setCreateTaskData: React.Dispatch<React.SetStateAction<CreateTaskType>>;
   setShowCreateTask: React.Dispatch<React.SetStateAction<boolean>>;
-  tasks: StructuredTaskType[];
 };
 
 const TimeBox = ({
@@ -22,14 +16,13 @@ const TimeBox = ({
   setShowCreateTask,
   timeInterval,
   day,
-  tasks,
 }: TimeBoxProps) => {
   return (
     <>
       <div
         data-type="timebox"
         data-date={day.getDate()}
-        data-month={day.getMonth()}
+        data-month={day.getMonth() + 1}
         data-year={day.getFullYear()}
         onClick={(e) => {
           const nativeElement = e.nativeEvent.target as HTMLElement;
@@ -45,19 +38,7 @@ const TimeBox = ({
         className={`h-4 timebox ${
           timeInterval.end % 60 === 0 && "border-b-[1px] border-neutral-100 "
         } relative`}
-      >
-        {tasks.length > 0 &&
-          tasks.map((task) => {
-            if (task.startTime === timeInterval.start) {
-              return (
-                <TaskComponent
-                  key={task.id}
-                  task={task as StructuredTaskType}
-                />
-              );
-            }
-          })}
-      </div>
+      ></div>
     </>
   );
 };
